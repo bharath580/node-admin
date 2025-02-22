53e6f9dab21b68c958e69ab3f377c9fd9dd76541
bharath580 <bharath@kabadiwallaconnect.in>
2025-02-22 09:46:40 +0530

    traceability and quality

--- router/dashboard.router.js
+++ router/dashboard.router.js
@@ -90,6 +90,59 @@ const baleTrend=`SELECT DATE(bales.created_on) as bale_date,sum(bales_details.qu
 join bales_details on bales.bale_id=bales_details.bale_id
 where bales.created_on between '${formattedStartDate}' and '${formattedEndDate}'
 group by bale_date order by bale_date;`
+const baleVolumeByMaterial=`SELECT baling_material.name as material,round(SUM(bales_details.quantity),0) AS quantity FROM bales
+join bales_details on bales.bale_id=bales_details.bale_id
+join baling_material on bales.bale_material=baling_material.id
+ WHERE bales.created_on>= '${formattedStartDate}' 
+      AND bales.created_on < DATE_ADD('${formattedEndDate}', INTERVAL 1 DAY)
+      group by baling_material.id,baling_material.name;`
+
+const traceability =`select distinct purchase_order.po_id,
+ IF(purchase_order.procurement_mode=1, IF(purchase_order.supplier_signature IS NOT NULL AND purchase_order.supplier_signature != '' AND 
+ purchase_order.driver_signature IS NOT NULL AND purchase_order.driver_signature != '' AND
+ purchase_order.supervisor_signature IS NOT NULL AND purchase_order.supervisor_signature != '', 1,0),
+            IF(purchase_order.procurement_mode=2, IF(purchase_order.supplier_signature IS NOT NULL 
+          AND purchase_order.supplier_signature != ''  AND purchase_order.supervisor_signature IS NOT NULL AND purchase_order.supervisor_signature != '', 1,0),0)) AS traceability
+ from purchase_order join
+purchase_order_details on purchase_order_details.po_id = purchase_order.po_id
+join batches_details on batches_details.purchase_order_id=purchase_order.po_id
+join batches on batches.batch_id = batches_details.batch_id 
+join bales_details on bales_details.batch_id= batches.batch_id
+join bales on bales.bale_id=bales_details.bale_id
+join sales_details on sales_details.bale_id=bales.bale_id
+join sales on sales.sales_id = sales_details.sale_id
+WHERE sales.created_on BETWEEN '${formattedStartDate}' AND '${formattedEndDate}';
+`      
+const segregation_quantity=`select 
+sum(segregation.quantity) as total_quantity from purchase_order
+join purchase_order_details on purchase_order_details.po_id = purchase_order.po_id
+join batches_details on batches_details.purchase_order_id = purchase_order_details.po_id
+join bales_details on bales_details.batch_id = batches_details.batch_id
+join sales_details on sales_details.bale_id = bales_details.bale_id
+join sales on sales.sales_id=sales_details.sale_id
+join segregation on segregation.po_id = purchase_order.po_id
+join segregation_material on segregation.segregation_material_id = segregation_material.segregation_material_id
+WHERE sales.created_on BETWEEN '${formattedStartDate}' AND '${formattedEndDate}'`
+const segregation_loss=`SELECT 
+    SUM(segregation.quantity) AS total_quantity
+   
+FROM purchase_order
+    JOIN purchase_order_details ON purchase_order_details.po_id = purchase_order.po_id
+    JOIN batches_details ON batches_details.purchase_order_id = purchase_order_details.po_id
+    JOIN bales_details ON bales_details.batch_id = batches_details.batch_id
+    JOIN sales_details ON sales_details.bale_id = bales_details.bale_id
+    JOIN sales ON sales.sales_id = sales_details.sale_id
+    JOIN segregation ON segregation.po_id = purchase_order.po_id
+    JOIN segregation_material ON segregation.segregation_material_id = segregation_material.segregation_material_id
+WHERE 
+    sales.created_on BETWEEN '${formattedStartDate}' AND '${formattedEndDate}'
+    AND segregation_material.rejects = 1`
+       const segregationQuantity=await dashboardModel.executeQuery(segregation_quantity);
+       const segregationLoss=await dashboardModel.executeQuery(segregation_loss);
+       const quality = segregationLoss == null && segregationLoss !== 0 
+    ? Number(((segregationQuantity[0].total_quantity / segregationLoss[0].total_quantity) * 100).toFixed(2)) 
+    : 100;
+
        result.procured_volume = await dashboardModel.executeQuery(volume);
        result.segregated = await dashboardModel.executeQuery(segregated);
        result.sold_volume = await dashboardModel.executeQuery(sold);
@@ -100,7 +153,17 @@ group by bale_date order by bale_date;`
        result.purchaseTrend = await dashboardModel.executeQuery(purchaseTrend);
        result.segregationTrend = await dashboardModel.executeQuery(segregationTrend);
        result.baleTrend = await dashboardModel.executeQuery(baleTrend);
-       
+       result.baleVolumeByMaterial = await dashboardModel.executeQuery(baleVolumeByMaterial);
+       result.traceability = await dashboardModel.executeQuery(traceability);
+       const traceableOrders = result.traceability.reduce((sum, item) => sum + (item.traceability || 0), 0);
+       console.log('traceableOrders',traceableOrders)
+       const totalOrders = result.traceability.length;
+       console.log(totalOrders);;
+       result.traceabilityValue = totalOrders > 0 
+    ? parseFloat(((traceableOrders / totalOrders) * 100).toFixed(2)) 
+    : 0;
+      result.quality=quality;
+      console.log('quality',segregationLoss)
       res.json(result);
     } catch (e) {
       console.error("Error:", e);
